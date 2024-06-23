6ef414dfb783481f59c1dc6fb33ada505ffe26fd
AladdineDev <aaldn.dev@gmail.com>
2024-06-23 23:53:05 +0200

    feat: search bar

--- app/src/main/java/com/example/accountmanager/ui/screen/AccountListScreen.kt
+++ app/src/main/java/com/example/accountmanager/ui/screen/AccountListScreen.kt
@@ -1,15 +1,18 @@
 package com.example.accountmanager.ui.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
@@ -23,6 +26,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -31,6 +35,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -44,6 +49,10 @@ fun AccountListScreen(
     viewModel: AccountViewModel = viewModel()
 ) {
     val accounts by viewModel.accounts.observeAsState(emptyList())
+    val searchResults by viewModel.searchResults.observeAsState(emptyList())
+
+    var query by remember { mutableStateOf(TextFieldValue("")) }
+    var lastQuery by remember { mutableStateOf("") }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -68,10 +77,32 @@ fun AccountListScreen(
                 .padding(innerPadding)
                 .padding(horizontal = 16.dp)
         ) {
+            TextField(
+                value = query,
+                onValueChange = {
+                    query = it
+                    val newQuery = it.text
+                    if (newQuery == query.text) {
+                        lastQuery = newQuery
+                        viewModel.searchAccounts(newQuery)
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                label = { Text("Search Accounts") },
+                singleLine = true,
+                trailingIcon = {
+                    IconButton(onClick = { query = TextFieldValue("") }) {
+                        Icon(Icons.Default.Clear, contentDescription = "Clear Search")
+                    }
+                },
+            )
+            Box(Modifier.height(16.dp))
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
             ) {
-                items(accounts) { account ->
+                val displayAccounts = if (query.text.isNotEmpty()) searchResults else accounts
+                items(displayAccounts) { account ->
                     AccountListItem(
                         account = account,
                         onEditClick = {
