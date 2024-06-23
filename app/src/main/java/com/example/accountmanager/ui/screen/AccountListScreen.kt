cc6f8b14e470c587fbd386dc4b9706dfab758605
AladdineDev <aaldn.dev@gmail.com>
2024-06-23 20:49:06 +0200

    style

--- app/src/main/java/com/example/accountmanager/ui/screen/AccountListScreen.kt
+++ app/src/main/java/com/example/accountmanager/ui/screen/AccountListScreen.kt
@@ -1,18 +1,21 @@
-
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -74,10 +77,16 @@ fun AccountListScreen(navController: NavHostController, viewModel: AccountViewMo
                 },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp),
+                    .padding(horizontal = 16.dp),
                 label = { Text("Search Accounts") },
                 singleLine = true,
+                trailingIcon = {
+                    IconButton(onClick = { query = TextFieldValue("") }) {
+                        Icon(Icons.Default.Clear, contentDescription = "Clear Search")
+                    }
+                },
             )
+            Box(Modifier.height(16.dp))
             LazyColumn(modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp)) {
                 val displayAccounts = if (query.text.isNotEmpty()) searchResults else accounts
                 items(displayAccounts) { account ->
